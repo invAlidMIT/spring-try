40211eec925558650f20eb54e71b457ff12347bc
Amit <amithegde777@gmail.com>
2024-11-18 12:31:40 +0530

    add files

---
+++ src/main/java/com/practice/App.java
@@ -0,0 +1,21 @@
+package com.practice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class,args);
+        System.out.println("hello");
+    }
+
+
+}
